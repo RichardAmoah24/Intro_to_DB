d4123b9a0525c68af4865d6645a0be2447a1ae4a
Richard_Amoah <amoahrichdad@gmail.com>
2025-10-19 14:49:07 +0000

    Add files via upload

---
+++ alx_book_store.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+create table Authors (
+author_id int auto_increment primary key,
+author_name varchar(215) not null
+);
+
+create table Books (
+book_id int auto_increment primary key,
+title varchar(130) not null,
+author_id int,
+price double not null,
+publication_date date,
+foreign key (author_id) references Authors(author_id)
+	on update cascade
+    on delete set null
+);
+create table Orders(
+	order_id int auto_increment primary key,
+    customer_id int,
+    order_date Date not null,
+    foreign key (customer_id) references Customers(customer_id)
+		on update cascade
+        on delete cascade
+);
+
+create table order_details (
+	ordertailid int auto_increment primary key,
+    order_id int,
+    book_id int,
+    quantity double not null check (quantity > 0), 
+    foreign key (order_id) references order(order_id)
+		on update cascade
+        on delete cascade,
+	foreign key (book_id) references Books(book_id)
+		on update cascade
+);
+
+insert into Authors (author_name) values
+('Chinua Achebe'),
+('Ama Ata Aidoo'),
+('Ngugi wa Thiong\'o');
+
+insert into Books (title, author_id, price, publication_date) Values
+('Things Fall Apart', 1, 49.99, '1958-06-17'),
+('Changs: A Love Story', 2, 39.50, '1991-01-01'), 
+('The River Between', 3, 45.00, '1965-03-15');
+
+insert into Customers (customer_name, email, address) values
+('John Doe', 'john.doe@example.com', '123 Accra Street, Ghana'),
+('Mary Johnson', 'mary.johnson@exmaple', '45 Kumasi Avenue, Ghana');
+
+insert into orders (customer_id, order_date, quantity) values
+(1, 1, 2),
+(1, 3, 1), 
+(2, 2, 1);
+
+
+
+
+    
+    
