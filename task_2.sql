70a7f77beb3a3faee5ac68c3ae745361a9745c5a
Richard_Amoah <amoahrichdad@gmail.com>
2025-10-19 19:54:12 +0000

    Add files via upload

---
+++ task_2.sql
@@ -0,0 +1,47 @@
+USE alx_book_store;
+CREATE TABLE IF NOT EXISTS Authors (
+author_id INT auto_increment PRIMARY KEY,
+author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Authors ( 
+	book_id INT AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+		ON UPDATE CASCADE
+        ON DELETE SET NULL
+	);
+    
+CREATE TABLE IF NOT EXISTS Customers (
+customer_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_name VARCHAR(215) UNIQUE NOT NULL, 
+address TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Orders (
+	order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+		ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Order_Datails (
+orderdetialid INT AUTO_INCREMENT PRIMARY KEY,
+order_id INT,
+book_id INT, 
+quality DOUBLE NOT NULL CHECK (quantity > 0), 
+FOREIGN KEY (order_id) REFERENCES Order(order_id)
+	ON UPDATE CASCADE
+    ON DELETE CASCADE,
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
+	ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+
+C
